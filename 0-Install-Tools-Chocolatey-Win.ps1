840b1f5ce45763a332091a8b32c60c2bb3cfb67f
tonikautto <toni@kautto.se>
2019-08-09 11:19:52 +1000

    Updated

--- 0-Install-Tools-Chocolatey-Win.ps1
+++ 0-Install-Tools-Chocolatey-Win.ps1
@@ -1,15 +1,20 @@
 #Requires -RunAsAdministrator
 
+# Install Chocolatey package manager
+# Reference https://chocolatey.org/install
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 # Virtual box
-choco install virtualbox --force --yes
+choco install virtualbox --yes
 
 # Minikube
-choco install minikube --force --yes
+choco install minikube --yes
 
 # Kubectl
-choco install kubernetes-cli --force --yes
+choco install kubernetes-cli --yes
 
 # Helm
-# choco install kubernetes-helm --yes
+#choco install kubernetes-helm --yes
 # Workaround due to issues with Helm 2.14, https://support.qlik.com/articles/000075385
 choco install kubernetes-helm --version 2.13.1 --yes --force
+
